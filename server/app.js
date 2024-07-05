97747ecc62063f498b3aa9b13ec28056c51ca2a0
Avish <avishmehta68@gmail.com>
2024-07-05 17:00:21 +0530

    audio saved to db

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const sqlite3 = require("sqlite3").verbose();
 const cors = require("cors");
+const multer = require("multer");
 const app = express();
 const port = process.env.PORT || 3000;
 const csvParser = require("csv-parser");
@@ -13,49 +14,56 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
+// Configure Multer for file uploads
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  }
+});
+const upload = multer({ storage: storage });
+
 // Create a connection to the SQLite database
 const db = new sqlite3.Database("../database/database.sqlite", (err) => {
   if (err) {
     console.error("Error opening database", err.message);
   } else {
     console.log("Connected to the SQLite database.");
-    // Create table if not exists
     db.run(`CREATE TABLE IF NOT EXISTS users (
-              id INTEGER PRIMARY KEY AUTOINCREMENT,
-              firstName TEXT,
-              lastName TEXT,
-              age INTEGER,
-              gender TEXT,
-              email TEXT
-            )`);
-
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      firstName TEXT,
+      lastName TEXT,
+      age INTEGER,
+      gender TEXT,
+      email TEXT
+    )`);
     db.run(`CREATE TABLE IF NOT EXISTS village_csv (
-              id INTEGER PRIMARY KEY AUTOINCREMENT,
-              name TEXT
-            )`);
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT
+    )`);
     db.run(`CREATE TABLE IF NOT EXISTS EnglishSentences (
-              id INTEGER PRIMARY KEY AUTOINCREMENT,
-              sentence TEXT
-            )`);
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      sentence TEXT
+    )`);
+    db.run(`CREATE TABLE IF NOT EXISTS audio_files (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      filename TEXT,
+      path TEXT
+    )`);
   }
 });
 
 // Route to submit user data
 app.post("/submit", (req, res) => {
   const { firstName, lastName, age, gender, email } = req.body;
-
-  // Insert submitted data into the database
   const sql = `INSERT INTO users (firstName, lastName, age, gender, email) VALUES (?, ?, ?, ?, ?)`;
   db.run(sql, [firstName, lastName, age, gender, email], function (err) {
     if (err) {
-      return console.error(
-        "Error inserting data into users table:",
-        err.message
-      );
+      return console.error("Error inserting data into users table:", err.message);
     }
-    console.log(
-      `A row has been inserted into users table with rowid ${this.lastID}`
-    );
+    console.log(`A row has been inserted into users table with rowid ${this.lastID}`);
     res.send("User data inserted successfully.");
   });
 });
@@ -63,29 +71,17 @@ app.post("/submit", (req, res) => {
 // Route to insert data from CSV file into the village_csv table
 app.get("/insert-csv-data", (req, res) => {
   const csvFilePath = "./village.csv";
-
-  // Check if the CSV file exists
   if (!fs.existsSync(csvFilePath)) {
     return res.status(404).send("CSV file not found.");
   }
-
-  // Read CSV file and insert data into village_csv table
   fs.createReadStream(csvFilePath)
     .pipe(csvParser())
     .on("data", (data) => {
-      // Insert data into SQLite database
-      db.run(
-        "INSERT INTO village_csv (name) VALUES (?)",
-        [data._0], // Adjust this to match the header of your CSV file
-        function (err) {
-          if (err) {
-            console.error(
-              "Error inserting data into village_csv table:",
-              err.message
-            );
-          }
+      db.run("INSERT INTO village_csv (name) VALUES (?)", [data._0], function (err) {
+        if (err) {
+          console.error("Error inserting data into village_csv table:", err.message);
         }
-      );
+      });
     })
     .on("end", () => {
       console.log("CSV file data inserted into village_csv table.");
@@ -95,37 +91,24 @@ app.get("/insert-csv-data", (req, res) => {
 
 app.get("/insert-sentence-csv-data", (req, res) => {
   const EnglishSentenceCSV = "./sentence_db.csv";
-
-  // Check if the CSV file exists
   if (!fs.existsSync(EnglishSentenceCSV)) {
     return res.status(404).send("CSV file not found.");
   }
-
-  // Read CSV file and insert data into EnglishSentenceCSV table
   fs.createReadStream(EnglishSentenceCSV)
     .pipe(csvParser())
     .on("data", (data) => {
-      // Insert data into SQLite database
-      console.log(data);
-
-      db.run(
-        "INSERT INTO  EnglishSentences (sentence) VALUES (?)",
-        [data.sentence], // Adjust this to match the header of your CSV file
-        function (err) {
-          if (err) {
-            console.error(
-              "Error inserting data into EnglishSentences table:",
-              err.message
-            );
-          }
+      db.run("INSERT INTO EnglishSentences (sentence) VALUES (?)", [data.sentence], function (err) {
+        if (err) {
+          console.error("Error inserting data into EnglishSentences table:", err.message);
         }
-      );
+      });
     })
     .on("end", () => {
       console.log("CSV file data inserted into EnglishSentences table.");
       res.send("CSV file data inserted into EnglishSentences table.");
     });
 });
+
 const getRandomIntegers = () => {
   const integers = [];
   for (let i = 0; i < 10; i++) {
@@ -136,47 +119,33 @@ const getRandomIntegers = () => {
 
 // Route to fetch data from the database based on random integers
 app.get("/fetch-data/:data_key", (req, res) => {
-  // Generate 5 random integers
   const randomIntegers = getRandomIntegers();
   const key = req.params.data_key;
   const database =
-    key === "words"
-      ? "village_csv"
-      : key === "sentence"
-      ? "EnglishSentences"
-      : res.json({});
-  // Construct SQL query to fetch data based on random integers
-  const sql = `SELECT * FROM ${database} WHERE id IN (${randomIntegers.join(
-    ","
-  )})`;
-
-  // Execute SQL query
+    key === "words" ? "village_csv" : key === "sentence" ? "EnglishSentences" : res.json({});
+  const sql = `SELECT * FROM ${database} WHERE id IN (${randomIntegers.join(",")})`;
   db.all(sql, [], (err, rows) => {
     if (err) {
       console.error("Error executing query:", err.message);
       return res.status(500).send("Internal server error");
     }
-    // Send the fetched data as response
     res.json(rows);
   });
 });
-app.get("/fetch-data", (req, res) => {
-  // Generate 5 random integers
-  const randomIntegers = getRandomIntegers();
 
-  // Construct SQL query to fetch data based on random integers
-  const sql = `SELECT * FROM EnglishSentences WHERE  id IN (${randomIntegers.join(
-    ","
-  )})`;
-
-  // Execute SQL query
-  db.all(sql, [], (err, rows) => {
+// Route to handle audio file upload
+app.post("/upload", upload.single('audio_data'), (req, res) => {
+  const { file } = req;
+  if (!file) {
+    return res.status(400).send("No file uploaded.");
+  }
+  const sql = `INSERT INTO audio_files (filename, path) VALUES (?, ?)`;
+  db.run(sql, [file.filename, file.path], function (err) {
     if (err) {
-      console.error("Error executing query:", err.message);
-      return res.status(500).send("Internal server error");
+      return console.error("Error inserting data into audio_files table:", err.message);
     }
-    // Send the fetched data as response
-    res.json(rows);
+    console.log(`A row has been inserted into audio_files table with rowid ${this.lastID}`);
+    res.send("Audio file uploaded successfully.");
   });
 });
 
--- server/public/assets/js/app.js
+++ server/public/assets/js/app.js
@@ -21,78 +21,39 @@ pauseButton.addEventListener("click", pauseRecording);
 function startRecording() {
   console.log("recordButton clicked");
 
-  /*
-		Simple constraints object, for more advanced audio features see
-		https://addpipe.com/blog/audio-constraints-getusermedia/
-	*/
-
   var constraints = { audio: true, video: false };
-
-  /*
-    	Disable the record button until we get a success or fail from getUserMedia() 
-	*/
 
   recordButton.disabled = true;
   stopButton.disabled = false;
   pauseButton.disabled = false;
 
-  /*
-    	We're using the standard promise based getUserMedia() 
-    	https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
-	*/
+  navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
+    console.log("getUserMedia() success, stream created, initializing Recorder.js ...");
 
-  navigator.mediaDevices
-    .getUserMedia(constraints)
-    .then(function (stream) {
-      console.log(
-        "getUserMedia() success, stream created, initializing Recorder.js ..."
-      );
+    audioContext = new AudioContext();
+    document.getElementById("formats").innerHTML =
+      "Format: 1 channel pcm @ " + audioContext.sampleRate / 1000 + "kHz";
 
-      /*
-			create an audio context after getUserMedia is called
-			sampleRate might change after getUserMedia is called, like it does on macOS when recording through AirPods
-			the sampleRate defaults to the one set in your OS for your playback device
+    gumStream = stream;
+    input = audioContext.createMediaStreamSource(stream);
 
-		*/
-      audioContext = new AudioContext();
+    rec = new Recorder(input, { numChannels: 1 });
+    rec.record();
 
-      //update the format
-      document.getElementById("formats").innerHTML =
-        "Format: 1 channel pcm @ " + audioContext.sampleRate / 1000 + "kHz";
-
-      /*  assign to gumStream for later use  */
-      gumStream = stream;
-
-      /* use the stream */
-      input = audioContext.createMediaStreamSource(stream);
-
-      /* 
-			Create the Recorder object and configure to record mono sound (1 channel)
-			Recording 2 channels  will double the file size
-		*/
-      rec = new Recorder(input, { numChannels: 1 });
-
-      //start the recording process
-      rec.record();
-
-      console.log("Recording started");
-    })
-    .catch(function (err) {
-      //enable the record button if getUserMedia() fails
-      recordButton.disabled = false;
-      stopButton.disabled = true;
-      pauseButton.disabled = true;
-    });
+    console.log("Recording started");
+  }).catch(function (err) {
+    recordButton.disabled = false;
+    stopButton.disabled = true;
+    pauseButton.disabled = true;
+  });
 }
 
 function pauseRecording() {
   console.log("pauseButton clicked rec.recording=", rec.recording);
   if (rec.recording) {
-    //pause
     rec.stop();
     pauseButton.innerHTML = "Resume";
   } else {
-    //resume
     rec.record();
     pauseButton.innerHTML = "Pause";
   }
@@ -101,21 +62,15 @@ function pauseRecording() {
 function stopRecording() {
   console.log("stopButton clicked");
 
-  //disable the stop button, enable the record too allow for new recordings
   stopButton.disabled = true;
   recordButton.disabled = false;
   pauseButton.disabled = true;
 
-  //reset button just in case the recording is stopped while paused
   pauseButton.innerHTML = "Pause";
 
-  //tell the recorder to stop the recording
   rec.stop();
-
-  //stop microphone access
   gumStream.getAudioTracks()[0].stop();
 
-  //create the wav blob and pass it on to createDownloadLink
   rec.exportWAV(createDownloadLink);
 }
 
@@ -125,28 +80,19 @@ function createDownloadLink(blob) {
   var li = document.createElement("li");
   var link = document.createElement("a");
 
-  //name of .wav file to use during upload and download (without extendion)
   var filename = new Date().toISOString();
 
-  //add controls to the <audio> element
   au.controls = true;
   au.src = url;
 
-  //save to disk link
   link.href = url;
-  link.download = filename + ".wav"; //download forces the browser to donwload the file using the  filename
+  link.download = filename + ".wav";
   link.innerHTML = "Save to disk";
 
-  //add the new audio element to li
   li.appendChild(au);
-
-  //add the filename to the li
   li.appendChild(document.createTextNode(filename + ".wav "));
-
-  //add the save to disk link to li
   li.appendChild(link);
 
-  //upload link
   var upload = document.createElement("a");
   upload.href = "#";
   upload.innerHTML = "Upload";
@@ -154,17 +100,21 @@ function createDownloadLink(blob) {
     var xhr = new XMLHttpRequest();
     xhr.onload = function (e) {
       if (this.readyState === 4) {
-        console.log("Server returned: ", e.target.responseText);
+        if (xhr.status === 200) {
+          alert("Upload successful!");
+          location.reload();
+        } else {
+          alert("Upload failed: " + xhr.responseText);
+        }
       }
     };
     var fd = new FormData();
-    fd.append("audio_data", blob, filename);
-    xhr.open("POST", "upload.php", true);
+    fd.append("audio_data", blob, filename + ".wav");
+    xhr.open("POST", "/upload", true);
     xhr.send(fd);
   });
-  li.appendChild(document.createTextNode(" ")); //add a space in between
-  li.appendChild(upload); //add the upload link to li
+  li.appendChild(document.createTextNode(" "));
+  li.appendChild(upload);
 
-  //add the li element to the ol
   recordingsList.appendChild(li);
 }
